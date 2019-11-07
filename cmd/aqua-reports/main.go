14b2b08b8b55f07063b3962174f0c6ad65937264
Bryan Morrow <bkmorrow@gmail.com>
2019-11-07 17:39:31 -0600

    add flag for command line arguments

--- cmd/aqua-reports/main.go
+++ cmd/aqua-reports/main.go
@@ -3,21 +3,62 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/BryanKMorrow/aqua-reports/src/system/app"
+	"github.com/BryanKMorrow/aqua-reports/src/system/aqua"
 )
 
 func main() {
-	// check for fatal, panic if true
-	fatal := checkEnv()
-	if fatal {
-		log.Fatalln("Environment variables not set, stopping aqua-reports")
+	var port string
+	// check if command line or container
+	if app.ModeFlag == "cli" {
+		log.Println("Using command line flags")
+		fatal := checkFlag()
+		if fatal {
+			log.Fatalln("Command line arguments are not set, stopping aqua-reports")
+		}
+		aqua.Mode = app.ModeFlag
+		aqua.URL = app.URLFlag
+		aqua.User = app.UserFlag
+		aqua.Password = app.PasswordFlag
+		port = strconv.Itoa(app.PortFlag)
+	} else if app.ModeFlag == "container" {
+		log.Println("Using the environment variables")
+		fatal := checkEnv()
+		if fatal {
+			log.Fatalln("Environment variables not set, stopping aqua-reports")
+		}
+		port = os.Getenv("AQUA_REPORTS_PORT")
+	} else {
+		log.Fatalln("Command Line argument 'mode' not set to value of cli or container")
 	}
 
 	s := app.NewServer()
-	port := os.Getenv("AQUA_REPORTS_PORT")
+
 	s.Init(port)
 	s.Start()
+}
+
+func checkFlag() bool {
+	fatal := false
+	if app.URLFlag == "" {
+		log.Println("Please set the url argument to the Aqua CSP address")
+		fatal = true
+	}
+	if app.UserFlag == "" {
+		log.Println("Please set the user argument to the Aqua CSP user with API access")
+		fatal = true
+	}
+	if app.PasswordFlag == "" {
+		log.Println("Please set the password argument for the Aqua CSP API access user")
+		fatal = true
+	}
+	if app.PortFlag == 0 {
+		log.Println("Please set the port argument for this application")
+		fatal = true
+	}
+	return fatal
 }
 
 func checkEnv() bool {
